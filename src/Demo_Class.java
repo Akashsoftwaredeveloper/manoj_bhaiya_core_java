31ff2e36f6f65c1f66a6206b889cf69138d3e3ed
ATS <prathamrathod252@gmail.com>
2021-09-30 11:59:06 +0530

    variables in java

---
+++ src/Demo_Class.java
@@ -0,0 +1,9 @@
+public class Demo_Class {
+    public static void main(String[] args) {
+        System.out.println("This project name is Manoj bhaiya core java ");
+        System.out.println("In java, class name should always start from capital letter");
+        System.out.println("In java program we have to write the code capital in capital and small in small letter" );
+        System.out.println("Java is case sensitive language");
+        System.out.println("After this we will learn about methods in core java");
+    }
+}
---
+++ src/Variables_Demo.java
@@ -0,0 +1,30 @@
+public class Variables_Demo {
+    public static void main(String[] args) {
+        // DataType variable_name = value
+
+        // put String value
+        String name= "Java ";
+        System.out.println("Name="+name);
+        String book="Hound of the Baskervill";
+        System.out.println("Book ="+book);
+        String school= "M.s School";
+        System.out.println("School="+school);
+
+        // put integer value
+        int roll_no= 564;
+        System.out.println("roll no="+roll_no);
+        int age = 22;
+        System.out.println("Age ="+age);
+        int school_code= 342;
+        System.out.println("School code="+school_code);
+
+        // put double value
+        double price= 7654.45;
+        System.out.println("price="+price);
+        double salary= 5487.54;
+        System.out.println("Salary= "+salary);
+        double mobile_price= 4300;
+        System.out.println("Mobile price="+mobile_price);
+
+    }
+}
