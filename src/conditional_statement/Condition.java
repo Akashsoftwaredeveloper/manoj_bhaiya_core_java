ac1d3d73a53e5ea393aed81f64f9ac04b9b0e700
ATS <prathamrathod252@gmail.com>
2021-10-01 19:45:51 +0530

    multiples variables

---
+++ src/conditional_statement/Condition.java
@@ -0,0 +1,23 @@
+package conditional_statement;
+
+public class Condition {
+
+    public static void main(String[] args) {
+        // if (expression) {code}
+        // else {code}
+
+        int age= 16;
+        if (age>18){
+            System.out.println("This person is adult");
+        }else{
+            System.out.println("This person is not adult");
+        }
+
+        String city = "pune";
+        if (city=="delhi"){
+            System.out.println("This person is in pune");
+        }else{
+            System.out.println("This person is not in pune");
+        }
+    }
+}
