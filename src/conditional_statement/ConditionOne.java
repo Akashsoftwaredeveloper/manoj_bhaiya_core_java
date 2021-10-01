f68afa30f9b1a5327a0da0307ebdc35e0126e53d
ATS <prathamrathod252@gmail.com>
2021-10-01 19:49:33 +0530

    multiples variables

---
+++ src/conditional_statement/ConditionOne.java
@@ -0,0 +1,31 @@
+package conditional_statement;
+
+public class ConditionOne {//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+
+
+
+
+
+        public static void main(String[] args) {
+            int age = 16;
+            if (age > 18) {
+                System.out.println("This person is adult");
+            } else {
+                System.out.println("This person is not adult");
+            }
+
+            String city = "pune";
+            if (city == "delhi") {
+                System.out.println("This person is in pune");
+            } else {
+                System.out.println("This person is not in pune");
+            }
+
+        }
+    }
+
+
