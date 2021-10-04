93b27474f83f093a6a1cdbbcef421119fa428a56
ATS <prathamrathod252@gmail.com>
2021-10-04 19:45:36 +0530

    Logical operator

---
+++ src/loops/Demo_loops.java
@@ -0,0 +1,24 @@
+package loops;
+
+public class Demo_loops {
+    public static void main(String[] args) {
+          //loops increment
+        for (int i=0; i<5;i++){
+
+            System.out.println("Printing Hello......."+i);
+        }
+        // loops decrement
+        for(int k=8; k>0; k--){
+            System.out.println("Hello SpringBoot....."+k);
+        }
+        // loops increment
+        for (int p=0; p<8;p++){
+            System.out.println("Printing value of p..."+p);
+        }
+        //  loops decrement
+        for(int q=2; q<8; q++) {
+
+            System.out.println("Printing core java" + q);
+        }
+    }
+}
---
+++ src/loops/Multiple_loops.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class Multiple_loops {
+    public static void main(String[] args) {
+
+        int two=2;
+
+        for(int i=1; i<=10;i++){
+            int output=2*i;
+            System.out.println("Printing table of two.."+output);
+        }
+        int three=3;
+        for (int k=1;k<10;k++){
+            int result=3*k;
+            System.out.println("Printing table of Three "+result);
+        }
+    }
+}
